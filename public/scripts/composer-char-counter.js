643256f291f46a7dd860cfc891c3c1b0e796e22b
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-21 11:38:51 -0400

    Add counter variable as suggested by AI feedback

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
   $('.new-tweet textarea').on('input', function() {
-    var maxLength = 140;
-    var currentLength = $(this).val().length;
-    var remaining = maxLength - currentLength;
+    let counter = $(this).closest('.new-tweet').find('.counter');
+    const maxLength = 140;
+    let currentLength = $(this).val().length;
+    let remaining = maxLength - currentLength;
 
     // Update the counter on the page
-    $('.new-tweet .counter').text(remaining);
+    $(counter).text(remaining);
 
     // Change the color if the text is too long
     if(remaining < 0) {
-      $('.new-tweet .counter').addClass('text-danger'); 
+      $(counter).addClass('text-danger'); 
     } else {
-      $('.new-tweet .counter').removeClass('text-danger');
+      $(counter).removeClass('text-danger');
     }
   });
 });
