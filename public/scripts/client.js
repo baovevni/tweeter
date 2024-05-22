241f94203579ec0e5c67592da445be71f10316ed
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-22 16:39:24 -0400

    Add loadTweets and timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,69 +1,62 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-$(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+const loadTweets = () => {
+  return $.get("/tweets")
+};
 
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    for (const tweet of tweets) {
-      // calls createTweetElement for each tweet
-      let singleTweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $('.tweet-container').append(singleTweet);
-    }
+const renderTweets = function(tweets) {
+  // loops through tweets
+  for (const tweet of tweets) {
+    // calls createTweetElement for each tweet
+    let singleTweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $('.tweet-container').append(singleTweet);
   }
+}
 
-  const createTweetElement = (data) => {
-    const tweet = $('<article>').addClass('tweet')
-    const tweetInfo = `<header class="tweet-header">
-  <div class="tweet-author">
-    <img src="${data.user.avatars}">
-    <p>${data.user.name}</p>
-  </div>
-  <p>${data.user.handle}</p>
+const createTweetElement = (data) => {
+  const tweet = $('<article>').addClass('tweet')
+  const tweetInfo = `<header class="tweet-header">
+<div class="tweet-author">
+  <img src="${data.user.avatars}">
+  <p>${data.user.name}</p>
+</div>
+<p>${data.user.handle}</p>
 </header>
 <div class="tweet-content">
-  <p>${data.content.text}</p>
+<p>${data.content.text}</p>
 </div>
 <footer class="tweet-footer">
-  <p>${data.created_at}</p>
-  <div>
-    <i class="fa-solid fa-heart"></i>
-    <i class="fa-solid fa-retweet"></i>
-    <i class="fa-solid fa-comment"></i>
-  </div>
+<p>${timeago.format(data.created_at)}</p>
+<div>
+  <i class="fa-solid fa-heart"></i>
+  <i class="fa-solid fa-retweet"></i>
+  <i class="fa-solid fa-comment"></i>
+</div>
 </footer>`
 
-    let tweetElement = tweet.append(tweetInfo);
-    return tweetElement;
-  }
+  let tweetElement = tweet.prepend(tweetInfo);
+  return tweetElement;
+}
 
-  renderTweets(data);
+const addPost = (formData) => {
+  return $.post("/tweets", formData);
+}
+
+$(document).ready(function() {
+
+  $("form").on("submit", (event) => {
+    event.preventDefault();
+    const formData = $(event.target).serialize();
+
+    addPost(formData)
+    .then((res) => {
+      console.log(res);
+      renderTweets([res])
+    })
+    .catch((err) => console.log(err));
+  })
+
+   loadTweets()
+  .then((data) => renderTweets(data))
+  .catch((err) => console.log("OH NO", err))
+
 });
